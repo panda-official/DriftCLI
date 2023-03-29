a2f21154fcd2e950973d636ef9e7e271bfa2ee62
Alexey Timin <atimin@gmail.com>
2023-03-29 17:15:47 +0200

    black

--- drift_cli/utils/helpers.py
+++ drift_cli/utils/helpers.py
@@ -46,12 +46,12 @@ def parse_path(path) -> Tuple[str, str]:
 
 
 async def read_topic(
-        pool: Executor,
-        client: DriftClient,
-        topic: str,
-        progress: Progress,
-        sem: Semaphore,
-        **kwargs,
+    pool: Executor,
+    client: DriftClient,
+    topic: str,
+    progress: Progress,
+    sem: Semaphore,
+    **kwargs,
 ):  # pylint: disable=too-many-locals
     """Read records from entry and show progress
     Args:
@@ -92,7 +92,9 @@ async def read_topic(
         loop.add_signal_handler(signal.SIGINT, stop_signal)
         loop.add_signal_handler(signal.SIGTERM, stop_signal)
     except NotImplementedError:
-        error_console.print("Signals are not supported on this platform. No graceful shutdown possible.")
+        error_console.print(
+            "Signals are not supported on this platform. No graceful shutdown possible."
+        )
 
     packages = await loop.run_in_executor(
         pool, client.get_package_names, topic, start, stop
@@ -108,7 +110,7 @@ async def read_topic(
 
     packages = sorted(packages)
     for i in range(0, len(packages), parallel):
-        tasks = [_read_package(p) for p in packages[i: i + parallel]]
+        tasks = [_read_package(p) for p in packages[i : i + parallel]]
         results = await asyncio.gather(*tasks)
 
         for drift_pkg in results:
@@ -120,7 +122,7 @@ async def read_topic(
                 progress.update(
                     task,
                     description=f"Topic '{topic}' "
-                                f"(copied {count} packages ({pretty_size(exported_size)}), stopped",
+                    f"(copied {count} packages ({pretty_size(exported_size)}), stopped",
                     refresh=True,
                 )
                 return
@@ -141,8 +143,8 @@ async def read_topic(
             progress.update(
                 task,
                 description=f"Topic '{topic}' "
-                            f"(copied {count} packages ({pretty_size(exported_size)}), "
-                            f"speed {pretty_size(speed)}/s)",
+                f"(copied {count} packages ({pretty_size(exported_size)}), "
+                f"speed {pretty_size(speed)}/s)",
                 advance=timestamp - last_time,
                 refresh=True,
             )
